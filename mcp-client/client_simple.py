8a2b31e72a77c52ae3b78191da81c1269a3d9dba
PajaritoMoyqi <pajaritomoyqi@gmail.com>
2026-02-24 17:29:13 +0900

    Lecture 46. Add client_simple.py code to make basic MCP client which only list tools of the connected MCP server

---
+++ mcp-client/client_simple.py
@@ -0,0 +1,31 @@
+from mcp import ClientSession, StdioServerParameters, types
+from mcp.client.stdio import stdio_client
+import asyncio
+import traceback
+
+server_params = StdioServerParameters(
+    command="npx",
+    args=["-y", "@openbnb/mcp-server-airbnb", "--ignore-robots-txt"],  # Optional command line arguments
+)
+
+async def run():
+    try:
+        print("Starting stdio_client...")
+        async with stdio_client(server_params) as (read, write):
+            print("Client connected, creating session...")
+            async with ClientSession(read, write) as session:
+
+                print("Initializing session...")
+                await session.initialize()
+
+                # TOOLS
+                print("Listing tools...")
+                tools = await session.list_tools()
+                print("Available tools:", tools)
+
+    except Exception as e:
+        print("An error occurred:")
+        traceback.print_exc()
+
+if __name__ == "__main__":
+    asyncio.run(run())
